9403e8954886a3612b4174fb8568755db318eee7
Othávio_Nakamura <103123718+othavionakamura@users.noreply.github.com>
2022-09-26 10:52:33 -0300

    26/09

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const list = document.querySelectorAll('.list');
-        function activeLink() {
-            list.forEach((item) =>
-                item.classList.remove('active'));
-            this.classList.add('active');
-        }
-        list.forEach((item) =>
-            item.addEventListener('click', activeLink));
-            
+function activeLink() {
+    list.forEach((item) =>
+        item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+    item.addEventListener('click', activeLink));
+
 
 const botaoAnimar = document.getElementById("animar");
 const caixa = document.querySelector(".caixa");
 
-botaoAnimar.addEventListener("click", ()=> {
+botaoAnimar.addEventListener("click", () => {
     caixa.style.animation = "";
     setTimeout(() => caixa.style.animation = "deslizar 1s linear", 5);
 });
