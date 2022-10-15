e541c4005c898e3af5f2b87dd31879bdc04578b6
Othavio Nakamura <103123718+othavionakamura@users.noreply.github.com>
2022-10-15 13:38:58 -0300

    14/10

--- Backend/ONG/script.js
+++ Backend/ONG/script.js
@@ -1,3 +1,40 @@
+const inputFile_perfil = document.querySelector('#picture_input_perfil');inputFile_perfil
+const pictureImage_perfil = document.querySelector('.picture_image_perfil');
+const pictureImageTxt_perfil = "<img src='boneco_perfil.jpg'>";
+pictureImage_perfil.innerHTML = pictureImageTxt_perfil;
+
+inputFile_perfil.addEventListener('change', function (p) {
+    const inputTarget_perfil = p.target;
+    const file_perfil = inputTarget_perfil.files[0];
+
+    if (file_perfil) {
+        const reader_perfil = new FileReader();
+
+        reader_perfil.addEventListener('load', function (p) {
+            const readerTarget_perfil = p.target;
+
+            const img_perfil = document.createElement('img');
+            img_perfil.src = readerTarget_perfil.result;
+            img_perfil.classList.add('picture_image_perfil');
+
+            pictureImage_perfil.innerHTML = '';
+
+            pictureImage_perfil.appendChild(img_perfil);
+        })
+
+        reader_perfil.readAsDataURL(file_perfil);
+    } else {
+        pictureImage_perfil.innerHTML = pictureImageTxt_perfil;
+    }
+})
+
+
+
+
+
+
+
+
 const inputFile = document.querySelector('#picture_input');
 const pictureImage = document.querySelector('.picture_image');
 const pictureImageTxt = 'Adicionar uma Imagem +';
@@ -27,14 +64,6 @@ inputFile.addEventListener('change', function (e) {
         pictureImage.innerHTML = pictureImageTxt;
     }
 })
-
-
-
-
-
-
-
-
 
 
 
@@ -80,16 +109,6 @@ inputFile2.addEventListener('change', function (w) {
 
 
 
-
-
-
-
-
-
-
-
-
-
 const inputFile3 = document.querySelector('#picture_input3');
 const pictureImage3 = document.querySelector('.picture_image3');
 const pictureImageTxt3 = 'Adicionar uma Imagem +';
@@ -117,5 +136,41 @@ inputFile3.addEventListener('change', function (t) {
         reader3.readAsDataURL(file3);
     } else {
         pictureImage3.innerHTML = pictureImageTxt3;
+    }
+})
+
+
+
+
+
+
+
+const inputFile4 = document.querySelector('#picture_input4');
+const pictureImage4 = document.querySelector('.picture_image4');
+const pictureImageTxt4 = 'Adicionar uma Imagem +';
+pictureImage4.innerHTML = pictureImageTxt4;
+
+inputFile4.addEventListener('change', function (y) {
+    const inputTarget4 = y.target;
+    const file4 = inputTarget4.files[0];
+
+    if (file4) {
+        const reader4 = new FileReader();
+
+        reader4.addEventListener('load', function (y) {
+            const readerTarget4 = y.target;
+
+            const img4 = document.createElement('img');
+            img4.src = readerTarget4.result;
+            img4.classList.add('picture_image4');
+
+            pictureImage4.innerHTML = '';
+
+            pictureImage4.appendChild(img4);
+        })
+
+        reader4.readAsDataURL(file4);
+    } else {
+        pictureImage4.innerHTML = pictureImageTxt4;
     }
 })
