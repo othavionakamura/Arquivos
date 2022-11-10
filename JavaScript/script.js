ca7c3024f990685265bf5a508ee2b459d4fb122c
Othávio_Nakamura <103123718+othavionakamura@users.noreply.github.com>
2022-11-10 15:04:28 -0300

    10/11

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -34,10 +34,10 @@ setTimeout(function() {
     console.log(
         setInterval (() => {
             const computedStyle = getComputedStyle(progressBar)
-            const width = parseFloat(computedStyle.getPropertyValue('--width')) || 10
+            const width = parseFloat(computedStyle.getPropertyValue('--width')) || 100
         
-            progressBar.style.setProperty('--width', width + .2)
-        }, 0)
+            progressBar.style.setProperty('--width', width + 10)
+        }, 10)
     )
 }, 1000)
 
